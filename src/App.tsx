0f17d71e67b21a3558ed37f40875bfa268da3a72
MohammadhRimaz <mohammadhrimaz@gmail.com>
2026-01-01 20:42:06 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Segoe UI, sans-serif",
+      }}
+    >
+      <h1>Hardware Store POS</h1>
+    </div>
+  );
+}
+
+export default App;
